c349e271f11adf00a79190d9e0e54dfcc5736696
alijradii <ali.jradi.dev@gmail.com>
2024-11-09 08:10:01 +0200

    added login logic based on email

---
+++ backend/api/login.php
@@ -0,0 +1,25 @@
+<?php
+
+include __DIR__ . '/../database/connection.php';
+
+$email = isset($_POST["email"]) ? $_POST["email"] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+
+if ($password && $email) {
+  $checkQuery = $connection->prepare("SELECT * FROM users WHERE email = ?");
+  $checkQuery->bind_param("s", $email);
+  $checkQuery->execute();
+  $checkResult = $checkQuery->get_result();
+
+  if ($checkResult->num_rows > 0) {
+    $user = $checkResult->fetch_assoc();
+    $userId = $user["id"];
+    echo json_encode(["status" => "success", "message" => "Logged in successfully", "user_id" => $userId]);
+  } else {
+    echo json_encode(["status" => "fail", "message" => "Email not found"]);
+  }
+} else {
+  echo json_encode(["status" => "fail", "message" => "invalid input"]);
+}
+
+$connection->close();
