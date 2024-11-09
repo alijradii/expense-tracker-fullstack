b0320852ebe8c3c316ac2ed564efe0a50879b0e8
alijradii <ali.jradi.dev@gmail.com>
2024-11-09 08:23:53 +0200

    added the getTransactions logic for finding the user based on user id

--- backend/api/getTransactions.php
+++ backend/api/getTransactions.php
@@ -4,3 +4,21 @@ include __DIR__ . '/../database/connection.php';
 
 $userId = $_GET["id"];
 $password = $_POST["password"];
+
+if (!isset($userId) || !isset($password)) {
+  echo json_encode(["status" => "fail", "message" => "invalid input"]);
+  exit;
+}
+
+$checkUserQuery = $connection->prepare("SELECT * FROM users WHERE id = ?");
+$checkUserQuery->bind_param("i", $userId);
+
+$checkUserQuery->execute();
+$checkUserResult = $checkUserQuery->get_result();
+
+if ($checkUserResult->num_rows === 0) {
+  echo json_encode(["status" => "fail", "message" => "user not found"]);
+  exit;
+}
+
+echo json_encode(["status" => "success", "message" => "found the user"]);
